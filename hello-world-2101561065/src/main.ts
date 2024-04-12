9b0a04265eb4ecab4702d3da7449c20b7cee6052
nmargenov <nikolay_margenov@yahoo.com>
2024-04-12 19:36:18 +0300

    Deleted[D some files

--- hello-world-2101561065/src/main.ts
+++ hello-world-2101561065/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+
+export const appConfig = {
+  providers: [provideRouter([])]
+};
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
